5c69181b931d6548a0476afab81442119070b2b5
astroJeff <andrews@astro.columbia.edu>
2017-02-08 20:47:17 +0200

    Added script to download spectra from Kepler et al 2015

---
+++ src/get_spectra.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import astropy
+from astropy.table import Table, Column, MaskedColumn
+
+from astroquery.vizier import Vizier
+from astroquery.sdss import SDSS
+
+# Get catalog from vizier search
+catalog_list = Vizier.find_catalogs('New white dwarf SDSS DR12')
+
+# Make sure we have the correct catalog
+print({k:v.description for k,v in catalog_list.items()})
+
+# Get the entire catalog, not just the first 50 rows
+Vizier.ROW_LIMIT = -1
+catalogs = Vizier.get_catalogs(catalog_list.keys())
+
+
+# cat = 'J/MNRAS/455/3413/table6'
+PMF = catalogs[0]['PMF']
+
+
+print "Downloading " + str(len(PMF)) + " spectra now..."
+
+for i in np.arange(len(PMF)):
+
+    plate = int(str(PMF[i])[0:4])
+    mjd = int(str(PMF[i])[5:10])
+    fiber = int(str(PMF[i])[11:15])
+
+    spec = SDSS.get_spectra(plate=plate, mjd=mjd, fiberID=fiber)
+
+    filename = '../data/spectra-'+str(PMF[i])+'.fits'
+    spec[0].writeto(filename)
+
+print "...finished downloading spectra."
